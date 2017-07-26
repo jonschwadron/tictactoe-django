130ce656dbcf065c3fe6e2874ae056974d465e50
jonschwadron <jonschwadron@gmail.com>
2017-07-26 14:31:17 -0500

    wsgi fix

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 
