ae9d89719c91bf9c89f8ba451d0b903f1f0b9ebe
jonschwadron <jonschwadron@gmail.com>
2017-07-26 14:19:49 -0500

    removed environ.setdefault

--- project/wsgi.py
+++ project/wsgi.py
@@ -15,6 +15,6 @@ from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 
